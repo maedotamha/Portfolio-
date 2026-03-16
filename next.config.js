d2c0e65d8d92fd3adca2fb6c10137af206ccc6f2
Maedot Amha <maedotamha@gmail.com>
2026-03-16 11:57:59 +0300

    folder changes

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        formats: ['image/avif', 'image/webp'],
-    },
-    // Suppress specific console logs in production if needed
-};
-
-module.exports = nextConfig;
